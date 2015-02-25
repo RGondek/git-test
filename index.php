6b544971ea4f751903f79665d930829149e89d6e
Rubens Gondek <rubens.gondek@gmail.com>
2015-02-25 10:08:08 -0300

    Modificado echo, adicionado style

--- index.php
+++ index.php
@@ -1,10 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Aula 02 PHP + GIT</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+        /*
+         * To change this template, choose Tools | Templates
+         * and open the template in the editor.
+         */
 
-  echo 12314;
-  echo 'vinicius';
-?>
+          echo 12314;
+          echo 'Rubens Gondek';
+        ?>
+    </body>
+</html>
