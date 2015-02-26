ad5c23dd8c0deb32a70e6da2d881f4dbddd84e26
41411978 <41411978@089896-P33S13.macklabs.br>
2015-02-26 11:29:17 -0300

    Aula 01 - Pronta

--- index.php
+++ index.php
@@ -6,15 +6,47 @@
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
+        <h2>Aula 01</h2>
         <?php
-
-        /*
-         * To change this template, choose Tools | Templates
-         * and open the template in the editor.
-         */
-
-          echo 12314;
-          echo 'Rubens Gondek';
+            $p1 = 8.0;
+            $m1 = 6.0;
+            $m2 = 9.0;
+            $proj1 = 6.0;
+            $proj2 = 6.0;
+            $proj3 = 6.0;
+            $proj4 = 6.0;
+            $trabF = 7.0;
+            $proc = 6.0;
+            $pF = 6.0;
+            
+            $mI = ((30*$p1) + (10*$m1) + (10*$m2) + (5*$proj1) + (5*$proj2) + (5*$proj3) + (5*$proj4) + (10*$trabF) + (20*$pF))/100;
+            $mF = 0.0;
+            
+            if ($mI >= 7.5) {
+                $mF = $mI;
+                echo "Aluno aprovado sem prova final!! <br> Parabéns!! Boas Férias!!";
+            }
+            else {
+                $mF = ($mI + $pF)/2;
+                if ($mF >= 5.0) {
+                    echo "Aprovado com prova final!! <br> Agora já pode descansar tranquilo.";
+                }
+                else {
+                    echo "Reprovado <br> Não foi dessa vez. Ano que vem tem mais TWII.";
+                }
+            }
+            echo "<br><br>Média intermediária: $mI <br> Média Final: $mF";
+        ?>
+        <br>
+        <hr>
+        <br>
+        <h2>Aula 02</h2>
+        <?php 
+            echo "<table><th><td>ID</td><td>NOME</td></th>";
+            for ($i = 0; $i < 5; $i++){
+                echo "<tr><td>id</td><td>nome</td></tr>";
+            }
+            echo "</table>";
         ?>
     </body>
 </html>
