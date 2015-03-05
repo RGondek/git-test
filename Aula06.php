c87eca64670eed6f55a466a93ad3f88b7c672d6d
Rubens <rubens.gondek@gmail.com>
2015-03-05 11:34:30 -0300

    Soma no operations.php

---
+++ Aula06.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+        <title>Aula 06</title>
+    </head>
+    <body>
+        <?php
+            include 'operations.php';
+            $n1; $n2;
+            echo "$n1 + $n2 = " + soma($n1, $n2); 
+        ?>
+    </body>
+</html>
---
+++ operations.php
@@ -0,0 +1,6 @@
+<?php
+    function soma($x, $y){
+        $res = $x + $y;
+        return $res;
+    }
+?>
