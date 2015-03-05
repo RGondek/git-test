e0fc5a41b09fb5571bba66dc33215d83d72aea08
Rubens <rubens.gondek@gmail.com>
2015-03-05 11:22:59 -0300

    Table array pronta!

--- Aula05.php
+++ Aula05.php
@@ -7,7 +7,8 @@
     <body>
         <?php
             $aulas = array(array("02K01", "Tecnologia Web II", "Segunda", "Charles"),array("02J01", "Linguagem de Programação I", "Terça", "João"),array("02K01", "Linguagem de Programação I", "Quarta", "Charles"));
-            echo "<table>";
+            echo "<table id='tbAula5'>";
+            echo "<tr><th>Turma</th><th>Disciplina</th><th>Semana</th><th>Professor</th></tr>";
                 for($i=0;$i< count($aulas);$i++){
                     echo "<tr>";
                     $vet = $aulas[$i];
